4bfa0de6e8621a9afbe5d6d9a73e390c603913d4
jianhao.dai <jianhao.dai@fraudmetrix.cn>
2017-05-21 12:14:59 +0800

    https -> http

--- modules/admin/views/layouts/main.php
+++ modules/admin/views/layouts/main.php
@@ -16,7 +16,7 @@ $this->cs->registerScript('fraudmetrix', '
     cimg.onload = function() {
         _fmOpt.imgLoaded = true;
     };
-    cimg.src = "https://fp.fraudmetrix.cn/fp/clear.png?partnerCode=" + _fmOpt.partner + "&appName=" + _fmOpt.appName + "&tokenId=" + _fmOpt.token;
+    cimg.src = "http://fp.fraudmetrix.cn/fp/clear.png?partnerCode=" + _fmOpt.partner + "&appName=" + _fmOpt.appName + "&tokenId=" + _fmOpt.token;
     var fm = document.createElement("script"); fm.type = "text/javascript"; fm.async = true;
     fm.src = ("https:" == document.location.protocol ? "https://" : "http://") + "static.fraudmetrix.cn/fm.js?ver=0.1&t=" + (new Date().getTime()/3600000).toFixed(0);
     var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(fm, s);
